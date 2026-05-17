5a82e34eac8cddba2566c4e236fd8699582a81c9
o1135839128h <o1135839128h@gmail.com>
2026-05-17 09:39:37 +0000

    used docker

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -5,14 +5,20 @@
   channel = "stable-24.11"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
   packages = [
-    # pkgs.go
+     pkgs.go
     # pkgs.python311
     # pkgs.python311Packages.pip
     # pkgs.nodejs_22
     # pkgs.nodePackages.nodemon
+    pkgs.docker
+    pkgs.docker-compose
+    pkgs.curl
   ];
   # Sets environment variables in the workspace
   env = {};
+  
+    services.docker.enable=true;
+  
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
